b2c264e44e85d5e59f5cf20a16fb01b880dca4bf
Austin Flatt <austindflatt@gmail.com>
2021-07-09 13:30:26 -0400

    created more classes and .cs files

---
+++ RPSLS/PlayerList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPSLS
+{
+    class PlayersList
+    {
+        public List<object> players = new List<object>();
+
+        public PlayersList()
+        {
+
+        }
+
+        public void AddPlayer(object player)
+        {
+            players.Add(player);
+        }
+
+        public void ListCount()
+        {
+            Console.WriteLine(players.Count);
+        }
+    }
+}
