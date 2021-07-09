5f4034a3e97593402e9d226fc753bd4be1fe7319
Austin Flatt <austindflatt@gmail.com>
2021-07-09 13:16:34 -0400

    add class

--- RPSLS/Program.cs
+++ RPSLS/Program.cs
@@ -6,7 +6,17 @@ namespace RPSLS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Human playerOne = new Human("Austin");
+            Human playerTwo = new Human("Playertwo");
+            CPU cpuPlayer = new CPU("Computer");
+            PlayersList playerList = new PlayersList();
+            playerList.AddPlayer(playerOne);
+            playerList.AddPlayer(playerTwo);
+            playerList.AddPlayer(cpuPlayer);
+            playerList.ListCount();
+            Game rpsls = new Game();
+            rpsls.WelcomeMessage();
+            rpsls.SelectMode();
         }
     }
 }
